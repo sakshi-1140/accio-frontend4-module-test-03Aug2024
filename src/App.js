15ab9eb6a98e8e17ea1b8a8312cdb4cdcfb38ad0
Sakshi <sakshichoudhary000@gmail.com>
2024-08-03 19:04:26 +0530

    browser router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; //check this in hashrouter
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; //check this in hashrouter
 import HomePage from './pages/HomePage';
 import DetailPage from './pages/DetailPage';
 
